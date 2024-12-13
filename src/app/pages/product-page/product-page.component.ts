16412b583ee9235273038857769ee96d1e716bf3
Paola_C <paola.cyprien@hetic.net>
2024-12-13 23:49:07 +0100

    page produit

--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-product-page',
@@ -9,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class ProductPageComponent {
 
+  console = console;
+
+  @ViewChild('imageSrc') imageSrc!: ElementRef;
+
+
+  listPhoto = [
+    "https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_1.jpg",
+    "https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_2.jpg",
+      "https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_3.jpg"
+      ,  "https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_4.jpg"
+ 
+
+
+  ]
+
+  constructor(){}
+
+
+  updatePreview(item: any){
+    this.imageSrc.nativeElement.src = item;
+  }
 }
